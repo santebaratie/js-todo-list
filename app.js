57d87a965c779b58b29dd1eeb5e40fa2c779a8ab
Muhibbulloh Hayitmurodov <181156023+santebaratie@users.noreply.github.com>
2025-01-13 23:07:58 +0500

    Add item to the page feature

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 "use strict";
+// Select UI elements
+
+const submitBtn = document.querySelector("input[type='submit'");
+const itemInput = document.querySelector("input[type='text']:first-child");
+const itemList = document.querySelector("ul");
+
+// Add element to the page
+
+function addItemToUI(item){
+
+  // Create list element items
+
+  const li = document.createElement('li');
+  const circle = document.createElement('i');
+  circle.classList.add('fa-regular', 'fa-circle');
+  const circleFilled = document.createElement('i');
+  circleFilled.classList.add('fa-solid','fa-circle', 'hidden');
+  const xMark = document.createElement('i');
+  xMark.classList.add('fa-solid', 'fa-xmark');
+  li.append(circle);
+  li.append(circleFilled)
+  li.append(` ${item}`);
+  li.append(xMark)
+  itemList.append(li);
+
+  // Clean input field after adding item to the page
+
+  itemInput.value = '';
+}
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const item = itemInput.value;
+  addItemToUI(item);
+});
